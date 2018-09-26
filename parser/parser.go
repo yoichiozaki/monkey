2e25bc2db00afcf37c055d546e99a72c11fb5711
zakimal <oza777you@gmail.com>
2018-09-26 12:52:06 +0900

    modify parser.go

--- parser/parser.go
+++ parser/parser.go
@@ -1,24 +1,38 @@
 package parser
 
 import (
+	"fmt"
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
-
+	l         *lexer.Lexer
+	errors    []string
 	curToken  token.Token // 今見ているトークン
 	peekToken token.Token // 次見るべきトークン
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -50,16 +64,18 @@ func (p *Parser) parseStatement() ast.Statement {
 	}
 }
 
+// let文をパースする
 func (p *Parser) parseLetStatement() *ast.LetStatement {
+	// let x = 5;
 	stmt := &ast.LetStatement{Token: p.curToken}
-	if !p.expectPeek(token.IDENT) {
+	if !p.expectPeek(token.IDENT) { // let = 5;みたいなやつはだめ
 		return nil
 	}
 	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
-	if !p.expectPeek(token.ASSIGN) {
+	if !p.expectPeek(token.ASSIGN) { // let x 5;みたいなやつはだめ
 		return nil
 	}
-	for !p.curTokenIs(token.SEMICOLON) {
+	for !p.curTokenIs(token.SEMICOLON) { // セミコロンまで読み飛ばす
 		p.nextToken()
 	}
 	return stmt
